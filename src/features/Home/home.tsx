874ebf6053f5a391c64e422d90bce2dbdfa87878
Philippe <borgodoff@gmail.com>
2024-04-01 14:55:21 +0200

    Dashboard

--- src/features/Home/home.tsx
+++ src/features/Home/home.tsx
@@ -1,8 +1,9 @@
-import Dashboard from "../Dashboard/dashboard";
+import Channel from "../Channel/channel";
+import { ChannelCard } from "../Channel/components/ChannelCard";
 
 export const Home = () => {
 
     return (
-        <Dashboard></Dashboard>
+        <Channel apiKey="QDuebCUy0G5U-oNCJX9lSTXbnxncDENUP66gY0_r7Le3lw4lW-9Y5n_vxQxNxWE8" userId={7}></Channel>
     );
 }
--- src/features/Login/login.tsx
+++ src/features/Login/login.tsx
@@ -1,6 +1,6 @@
 import { createEffect, createSignal, Setter } from "solid-js";
 import { Button, TextField } from "@suid/material";
-import SignInSide from "../SigninSide/signInSide";
+import SignInSide from "./components/signInSide";
 
 interface IParams {
     setApiKey: Setter<string | undefined>;
@@ -10,15 +10,10 @@ export default (props: IParams) => {
     const [formApiKey, setFormApiKey] = createSignal<string>('');
 
     createEffect((e) => {
-        props.setApiKey(formApiKey); console.log("api key updated ?", formApiKey());
+        props.setApiKey(formApiKey);
         });
 
     return (
         <SignInSide setFormApiKey={setFormApiKey}></SignInSide>
     )
 }
-
-/*<h2>Login using your API key</h2>
-<TextField id="apikey" label="enter your api key" variant="standard" onChange={(e) => setFormApiKey(e.currentTarget.value)} />
-            <Button variant="contained" onClick={e => props.setApiKey(formApiKey)}>Login with API Key</Button>
-            */
